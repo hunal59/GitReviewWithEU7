f489f3a9d5223f825b948e474629072b86be1df9
hunal <hsynemr59@gmail.com>
2021-12-19 22:51:23 +0300

    new github branch commit

---
+++ src/test/java/gitHubMergeFeature/Test01.java
@@ -0,0 +1,8 @@
+package gitHubMergeFeature;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        System.out.println("ı made these changing locally but ı will merge them at the github");
+    }
+}
