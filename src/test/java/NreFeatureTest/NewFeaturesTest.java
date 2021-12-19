ea7f49411b9481666149852eac336c62faa84499
hunal <hsynemr59@gmail.com>
2021-12-19 22:43:06 +0300

    third day in my project

---
+++ src/test/java/NreFeatureTest/NewFeaturesTest.java
@@ -0,0 +1,8 @@
+package NreFeatureTest;
+
+public class NewFeaturesTest {
+    public static void main(String[] args) {
+
+        System.out.println("ı am ");
+    }
+}
---
+++ src/test/java/NreFeatureTest/NewTest02.java
@@ -0,0 +1,8 @@
+package NreFeatureTest;
+
+public class NewTest02 {
+    public static void main(String[] args) {
+
+        System.out.println("some more changes");
+    }
+}
